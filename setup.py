372ff7db5d518cd50ace96c42da721692b90797f
Revant Nandgaonkar <revant.one@gmail.com>
2016-05-17 17:59:46 +0600

    bumped to version 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = '0.0.4'
+version = '0.0.5'
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
